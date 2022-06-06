0ca4db83dee24267463f9ac6f2716801f5fcfe76
Kai Wang <kaiuchihaa@gmail.com>
2022-06-06 14:09:48 -0300

    adicionado novo projeto ao portifólio

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ outros projetos
 [GATITOS](https://secret-garden-28387.herokuapp.com/login)
 [PETSHOP](https://hidden-coast-70894.herokuapp.com/)
 [TO-DO](https://tranquil-garden-45649.herokuapp.com/)
+[NPM status-link-md](https://www.npmjs.com/package/status-link-md)
--- src/Components/qualifications/Portifolio.tsx
+++ src/Components/qualifications/Portifolio.tsx
@@ -2,12 +2,14 @@ import "./Portifolio.css";
 import pets from "../props/apipetshop.png"
 import gatito from '../props/gatitos.png'
 import todo from '../props/todo.png'
+import linkmarkdown from '../props/linkmarkdown.png';
 
 const Portifolio = () => {
   return (
     <div className="container-portifolio-main">
       <h2 className="container-portifolio-titulo"> Meus Trabalhos </h2>
       <div className = "container-portifolio-cards">
+
         <div className="container-portifolio--card">
         <div>
         <figure>
@@ -22,8 +24,8 @@ const Portifolio = () => {
             <div  className="portifolio-button"><a href="https://hidden-coast-70894.herokuapp.com/" target={"blank"}>Acesse! </a></div>
         </div>
         </div>
-          
         </div>
+
         <div className="container-portifolio--card">
         <div>
         <figure>
@@ -54,10 +56,25 @@ const Portifolio = () => {
                 <p className="text">Abordagem moderna do React js utilizando conceitos modernos de React components e hooks, além de configurações e uso de lógica em Javascript para criação das funcionabilidades, React router dom para criação de rotas estáticas que ajudam na navegação do SPA sem necessidade de várias requisições aumentando a performance da página.</p>
                 <div  className="portifolio-button"><a href="https://tranquil-garden-45649.herokuapp.com/" target={"blank"}>Acesse! </a></div>
             </div>
-
         </div>
-        <div className="container-portifolio--card">
 
+        <div className="container-portifolio--card">
+          
+        <div>
+        <figure>
+              <img src={linkmarkdown} className="portifolio-img"  alt="api status-link-md"/>
+            </figure>
+            <div className="portifolio-text">
+           
+            <h2 className="title"> API status-link-md</h2>
+            <hr/>
+
+            <p className="text">API que verifica todos links em um arquivo markdown e retorna se o link está ativo com retorno 200 ou se o mesmo está fora do ar com retorno 404. Está API é util para blogs que usam o MarkDown para suas postagens, desta maneira com os retornos corretos os editores e aqueles que serão os autores do conteúdos conseguem garantir que os links estão funcionais para todos os usuários.</p>
+            <div  className="portifolio-button"><a href="https://www.npmjs.com/package/status-link-md" target={"blank"}>NPM </a></div>
+            <div  className="portifolio-button"><a href="https://github.com/xing-wang-kai/PROJETO_BACKEND_API_STATUSOFMARKDOWNLINK" target={"blank"}>GitHub </a></div>
+        </div>
+        </div>
+          
         </div>
         <div className="container-portifolio--card">
 
